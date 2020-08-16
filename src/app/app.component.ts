b2d207141b429d5624ef918725a9f70be3cefd48
philc511 <phil.capon@fastmail.co.uk>
2020-08-16 20:03:18 +0100

    adding dynamics forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { QuestionService } from './question-service';
+import { Observable } from 'rxjs';
+import { QuestionBase } from './question-base';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:  [QuestionService]
 })
 export class AppComponent {
+  questions$: Observable<QuestionBase<any>[]>;
+
+  constructor(service: QuestionService) {
+    this.questions$ = service.getQuestions();
+  }
   title = 'book-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AddBookComponent } from './add-book/add-book.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ListBooksComponent } from './list-books/list-books.component';
 import {StoreModule} from "@ngrx/store"
-import {reducer} from "./state/app.state"
+import {reducer} from "./state/app.state";
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AddBookComponent,
-    ListBooksComponent
+    ListBooksComponent,
+    DynamicFormQuestionComponent,
+    DynamicFormComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule, StoreModule.forRoot(reducer)
